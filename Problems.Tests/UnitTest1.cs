c42d417a34ea19f021cd3f122a4d418db21814ec
Mdumiseni Monamate <MonamateMD@gmail.com>
2020-02-24 21:19:23 +0200

    added tests

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -14,5 +14,20 @@ namespace Problems.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void TestOne(){
+            var ans1 = "5,1:hello";
+            var rslt1 = Prefix("hello");  
+
+            var ans2 = "0,0:";
+            var rslt2 = Prefix("");
+
+            var ans3 = "27,5:what ... did you say?? ";
+            var rslt3 = Prefix("what ... did you say?? ");
+
+            Assert.AreEqual(ans1, rslt1);
+
+        }
     }
 }
