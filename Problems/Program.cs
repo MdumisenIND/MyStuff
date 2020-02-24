0988879c71e8a145d977d40230f4afd0aed9258b
MdumisenIND <36520179+MdumisenIND@users.noreply.github.com>
2020-02-24 17:56:05 +0200

    q12

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,24 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
-    }
+
+        private string Prefix(string input)
+        {
+            if (input == null)
+                return null;
+
+            string len, numWords;
+
+            len = input.Length + ",";
+            numWords = input.Split(" ") + ":";
+
+            return (len + numWords + input);
+        }
+    }    
 }
